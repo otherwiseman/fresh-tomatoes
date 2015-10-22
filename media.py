98c71c6a46bf681c4a4267f5852eaf1006d7848f
laura mooney <laurajanemooney@gmail.com>
2015-10-21 17:30:12 -0700

    Update spacing in docstrings

--- media.py
+++ media.py
@@ -1,16 +1,13 @@
-""" This module defines the Movie class.
-"""
+""" This module defines the Movie class."""
 
 import webbrowser
 
 
 class Movie:
-    """
-    Supplies two functions: __init__() and show_trailer()
-    """
+    """ Supplies two functions: __init__() and show_trailer() """
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, spotify_playlist):
-        """defines instance methods for a single Movie
-        """
+        """ defines instance methods for a single Movie """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -18,7 +15,6 @@ class Movie:
         self.spotify_playlist_url = spotify_playlist
 
     def show_trailer(self):
-        """ :return: a Movie instance whose trailer should be played
-        """
+        """ :return: a Movie instance whose trailer should be played """
         webbrowser.open(self.trailer_youtube_url)
 
